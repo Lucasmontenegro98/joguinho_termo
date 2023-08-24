ac9fe9ed216b354e5cb2ee5afea0f03c43de9fc3
Lucas Montenegro <103197365+Lucasmontenegro98@users.noreply.github.com>
2023-08-24 16:00:48 -0300

    aula45.py

--- aula45.py
+++ aula45.py
@@ -1,22 +1,31 @@
-palavra_secreta = 'perfume'
+palavra_secreta = 'programar'
 letras_acertadas = ''
 while True:
 
-    letra_digitada = input('tente advinhar a senha secreta :')
+    letra_digitada = input('tente advinhar a palavra secreta :')
 
 
     
     if len(letra_digitada) > 1:
         print('digite apenas uma letra')
         continue
-
     
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
-
+    
+    palavra_formada = ''
+    
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+            palavra_formada += letra_secreta
 
-    else:
-        print('*')        
+        else:
+            palavra_formada += '*'      
+    print('palavra formada' , palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        print('parabéns voce acertou a palavra do dia')
+        print(f'a palavra formada era: {palavra_formada}')
+
+        break
+    
